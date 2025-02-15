6c4d640a1a230428b05ffbbad6c77ad304714990
novsiann <nikitaovsannik63@gmail.com>
2025-02-15 21:07:28 +0100

    small changes

--- frontend/src/routing/routes.ts
+++ frontend/src/routing/routes.ts
@@ -1,8 +1,9 @@
-import { homePage, gamePage, registrationPage, loginPage } from "../Layout/index"
-import { handleMainPage } from "../pages/mainPage"
+import { playPage, gamePage, registrationPage, loginPage, homePage } from "../Layout/index"
+import { handlePlayPage } from "../pages/playPage"
 import { handleGame } from "../pages/game"
 import { handleRegistration } from "../pages/registration"
 import { handleLogin } from "../pages/login"
+import { handleHomePage } from "../pages/home"
 
 interface handleFunctionI {
     layoutCreate: (mainWrapper?: HTMLDivElement) => string;
@@ -10,7 +11,8 @@ interface handleFunctionI {
 }
 
 export const routes: Record< string, handleFunctionI > = {
-    "/": { layoutCreate: homePage, handleFunc: handleMainPage},
+    "/": { layoutCreate: homePage, handleFunc: handleHomePage},
+    "/play": { layoutCreate: playPage, handleFunc: handlePlayPage},
     "/game":  { layoutCreate: gamePage, handleFunc: handleGame},
     "/registration": { layoutCreate: registrationPage, handleFunc: handleRegistration},
     "/login":  { layoutCreate: loginPage, handleFunc: handleLogin},
