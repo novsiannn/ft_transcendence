d6782a8c2b56f1d4b54015be68f5945c90e28452
novsiann <nikitaovsannik63@gmail.com>
2025-02-15 14:23:23 +0100

    handling a navigation page

--- frontend/src/nagivation/navigation.ts
+++ frontend/src/nagivation/navigation.ts
@@ -1,14 +1,42 @@
 import { navigateTo } from "../routing";
 import { naviRoutes } from "./navigationRoutes";
+import { dropMenuRoutes } from "./navigationRoutes";
 
 export function navigationHandle(){
-	let navigationBtns = document.querySelectorAll("#naviBtn");
+	const naviBtns = document.querySelectorAll("#naviBtn");
+	const profileBtn = document.querySelector("#profileIcon");
+	const dropdownMenu = document.querySelector("#dropdownMenu");
+	const naviDropMenuBtns = document.querySelectorAll("#dropdownMenu a");
+
 	
-	navigationBtns.forEach((btn) => {
-		btn.addEventListener('click', (e) => {
+	naviBtns.forEach((btn) => {
+		btn.addEventListener('click', () => {
 			if(btn.innerHTML.trim() in naviRoutes){
 				navigateTo(naviRoutes[btn.innerHTML.trim()]);	
 			}
 		});
 	})
+	
+	naviDropMenuBtns.forEach((btn) => {
+		btn.addEventListener('click', () => {
+			if(btn.innerHTML.trim() in dropMenuRoutes){
+				navigateTo(dropMenuRoutes[btn.innerHTML.trim()]);	
+			}
+		});
+	})
+
+	profileBtn!.addEventListener('click', () =>{
+		dropdownMenu!.classList.toggle("hidden");
+	});
+
+	document.addEventListener('click', (e) => {
+		if(e.target !== dropdownMenu && e.target !== profileBtn)
+			dropdownMenu!.classList.add("hidden");
+	})
+
+	document.addEventListener('keydown', (e) => {
+		if (e.keyCode == 27)
+			dropdownMenu!.classList.add("hidden");
+	})
+
 }
