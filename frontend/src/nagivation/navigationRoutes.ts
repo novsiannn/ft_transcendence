ab7e35cfeafe8de35a891d447d14f3e0f0c9d383
novsiann <nikitaovsannik63@gmail.com>
2025-02-15 14:23:53 +0100

    added new routes for a drop down menu

--- frontend/src/nagivation/navigationRoutes.ts
+++ frontend/src/nagivation/navigationRoutes.ts
@@ -3,4 +3,12 @@ export const naviRoutes: { [key: string]: string} = {
 	"Game": "/game",
 	"Registration" : "/registration",
 	"Login" : "/login"
+}
+
+export const dropMenuRoutes: { [key: string]: string} = {
+	"Profile": "/profile",
+	"Game": "/game",
+	"Leaderboard" : "/leaderboard",
+	"Settings" : "/settings",
+	"Logout" : "/logout"
 }  
