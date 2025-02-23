37f0e3848a3f56036a42e533c6fa1eb4005cd7a9
novsiann <nikitaovsannik63@gmail.com>
2025-02-23 23:02:30 +0100

    settings block added

--- frontend/src/Layout/settingsPage.ts
+++ frontend/src/Layout/settingsPage.ts
@@ -1,12 +1,14 @@
 import { navigation } from "../elements"
 
 export function settingsPage(mainWrapper: HTMLDivElement | undefined) {
+	document.body.classList.add("bg-gradient-to-t", "from-black", "via-black", "to-gray-800", "h-full");
+	mainWrapper!.className = "mx-auto h-screen w-full";
 	let res = `
 				${navigation()}
-				<div class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-t from-black via-black to-gray-800 space-y-8">
-					<div class="px-6 py-3 text-white bg-blue-500 rounded-lg text-xl hover:bg-blue-700 focus:outline-none" id="btn-play">
-						Settings!
-					</div>
+				<div class="w-full flex flex-col flex-1 items-center justify-center min-h-screen">
+  					<div class="px-6 py-3 text-black bg-white rounded-lg text-xl w-4/5 h-96 text-center">
+    					<h1>Settings!</h1>
+  					</div>
 				</div>`;
 	return res;
 }
