2742961acdb32b729c6c51148326a8ac12bd5835
kilchenk <kirill220420054004@gmail.com>
2025-02-25 17:09:27 +0100

    done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const fastify = require('fastify')({ logger: true });
+const fastify = require('fastify')({ logger: false });
 const sequelize = require('../db/database');
 const userRoutes = require('./routes/index');
 
