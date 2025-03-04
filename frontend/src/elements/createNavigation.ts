d90e2639ea235671d93c13cb88f047bb0f86116e
novsiann <nikitaovsannik63@gmail.com>
2025-03-04 14:58:58 +0100

    changed styles of buttons in navigation

--- frontend/src/elements/createNavigation.ts
+++ frontend/src/elements/createNavigation.ts
@@ -5,10 +5,10 @@ export function navigation() {
 
 
     		<div class="flex-1 flex justify-center space-x-8">
-       			<button class="font-mono h-5" id="naviBtn"> Home </button>
-        		<button class="font-mono h-5" id="naviBtn"> Game </button>
-        		<button class="font-mono h-5" id="naviBtn"> Registration </button>
-        		<button class="font-mono h-5" id="naviBtn"> Login </button>
+       			<button class="font-mono h-5 focus:outline-none transition delay-100 hover:text-green-500" id="naviBtn"> Home </button>
+        		<button class="font-mono h-5 focus:outline-none transition delay-100 hover:text-green-500" id="naviBtn"> Game </button>
+        		<button class="font-mono h-5 focus:outline-none transition delay-100 hover:text-green-500" id="naviBtn"> Registration </button>
+        		<button class="font-mono h-5 focus:outline-none transition delay-100 hover:text-green-500" id="naviBtn"> Login </button>
     		</div>
 
 
@@ -17,10 +17,10 @@ export function navigation() {
 
 
         		<div id="dropdownMenu" class="absolute right-0 mt-2 w-48 bg-white text-black shadow-lg rounded-lg p-2 hidden">
-            		<a class="block px-4 py-2 hover:bg-gray-200">Profile</a>
-					<a class="block px-4 py-2 hover:bg-gray-200">Game</a>
-					<a class="block px-4 py-2 hover:bg-gray-200">Leaderboard</a>
-            		<a class="block px-4 py-2 hover:bg-gray-200">Settings</a>
+            		<a class="block px-4 py-2 hover:bg-gray-200 focus:outline-none">Profile</a>
+					<a class="block px-4 py-2 hover:bg-gray-200 focus:outline-none">Game</a>
+					<a class="block px-4 py-2 hover:bg-gray-200 focus:outline-none">Leaderboard</a>
+            		<a class="block px-4 py-2 hover:bg-gray-200 focus:outline-none">Settings</a>
             		<a class="block px-4 py-2 text-red-600 hover:bg-gray-200">Logout</a>
         		</div>
     		</div>
