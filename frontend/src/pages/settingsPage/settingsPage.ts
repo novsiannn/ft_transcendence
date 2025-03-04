6b04ab385513783a65cafd2703e48172663330b4
novsiann <nikitaovsannik63@gmail.com>
2025-03-04 23:07:22 +0100

    reading value from inputs and sending it to server

--- frontend/src/pages/settingsPage/settingsPage.ts
+++ frontend/src/pages/settingsPage/settingsPage.ts
@@ -1,5 +1,37 @@
 import { navigationHandle } from "../../nagivation";
+import { userAPI } from "../../services/api";
 
 export function handleSettings() {
 	navigationHandle();
+	const inputs = document.querySelectorAll<HTMLInputElement>("#inputUserInfo");
+	const btnSave = document.querySelector<HTMLButtonElement>("#saveChangesSettings");
+	const profileImgContainer = document.querySelector<HTMLDivElement>("#profileImgContainer");
+	const uploadImgBtn = document.querySelector<HTMLInputElement>("#uploadImgBtn");	
+	
+	const data: Record<string, string> = {};
+
+	// uploadImgBtn!.addEventListener('change', async (event) => {
+	// 	const input = event.target as HTMLInputElement;
+		
+	// 	if(input.files!.length > 0){
+	// 		const imgFile = input.files![0];
+	// 		const isImageType = imgFile!.type.startsWith("image");
+	// 	}
+		
+	// })
+
+	btnSave!.addEventListener('click', () => {
+		try {
+			inputs.forEach((input) => {
+				if(!input.value)
+					throw new Error("empty area!");
+				data[input.placeholder.split(" ").join("")] = input.value;
+			})
+			userAPI.postDatas(data);
+		}
+		catch (error) {
+			alert(error)
+		}
+	});
+
 }
