35a9b6f0c5c9bed52f132b185b692c2d57ad242f
novsiann <nikitaovsannik63@gmail.com>
2025-03-09 23:21:55 +0100

    added route for leaderboard

--- frontend/src/routing/routes.ts
+++ frontend/src/routing/routes.ts
@@ -1,4 +1,4 @@
-import { playPage, gamePage, registrationPage, loginPage, homePage, profilePage, settingsPage, errorPage } from "../Layout/index"
+import { playPage, gamePage, registrationPage, loginPage, homePage, profilePage, settingsPage, errorPage, leaderBoardPage } from "../Layout/index"
 import { handlePlayPage } from "../pages/playPage"
 import { handleGame } from "../pages/game"
 import { handleRegistration } from "../pages/registration"
@@ -7,6 +7,7 @@ import { handleHomePage } from "../pages/home"
 import { handleProfilePage } from "../pages/profile"
 import { handleSettings } from "../pages/settingsPage"
 import { handleErrorPage } from "../pages/errorPage"
+import { handleLeaderboardPage } from "../pages/leaderboard"
 
 interface handleFunctionI {
     layoutCreate: (mainWrapper?: HTMLDivElement) => string;
@@ -21,5 +22,6 @@ export const routes: Record<string, handleFunctionI> = {
     "/registration": { layoutCreate: registrationPage, handleFunc: handleRegistration },
     "/login": { layoutCreate: loginPage, handleFunc: handleLogin },
     "/settings": {layoutCreate: settingsPage, handleFunc: handleSettings},
-    "/error": {layoutCreate: errorPage, handleFunc: handleErrorPage}
+    "/error": {layoutCreate: errorPage, handleFunc: handleErrorPage},
+    "/leaderboard": {layoutCreate: leaderBoardPage, handleFunc: handleLeaderboardPage},
 };
