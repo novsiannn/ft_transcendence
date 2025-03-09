1ba7fe161180b3029eef390f7f6ae4c59b5fbc24
novsiann <nikitaovsannik63@gmail.com>
2025-03-09 23:50:28 +0100

    moved filter func to handle folder

--- frontend/src/pages/leaderboard/leaderboard.ts
+++ frontend/src/pages/leaderboard/leaderboard.ts
@@ -1,5 +1,9 @@
 import { navigationHandle } from "../../nagivation";
 
+export const filterLeaderBoard = (data: Array<{ id: number, username: string, playedGames: number, wonGames: number, totalPoints: number }>) => {
+	return data.sort((a, b) => b.totalPoints - a.totalPoints).slice(0, 8);
+}
+
 export function handleLeaderboardPage() {
 	navigationHandle();
 }
