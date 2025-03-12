d0540e92645b799e736c7cb7f2da5b9a5d648fd7
kilchenk <kirill220420054004@gmail.com>
2025-03-12 20:45:20 +0100

    almost work

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,9 +1,31 @@
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
 
+// const sequelize = new Sequelize({
+//   dialect: "sqlite",
+//   storage: "./db/database.sqlite",
+//   logging: console.log,
+// });
+
+// module.exports = sequelize;
+
+
+const { Sequelize } = require("sequelize");
+const path = require("path");
+
+// Используйте переменную окружения или фиксированный путь
+const dbPath = process.env.DB_PATH || "/app/database.sqlite";
+
+console.log("Using database at path:", dbPath);
+
+// Настройка Sequelize с SQLite
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "./db/database.sqlite",
-  logging: console.log,
+  storage: dbPath,
+  logging: false,
+  dialectOptions: {
+    // Режим открытия базы данных SQLite
+    mode: parseInt('0666', 8) // Полные права на чтение/запись
+  }
 });
 
 module.exports = sequelize;
