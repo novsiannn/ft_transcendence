87f25389c7b175f4d0ce093c96f8b7226b87b6c2
novsiann <nikitaovsannik63@gmail.com>
2025-03-17 22:16:44 +0100

    add login btn

--- frontend/src/elements/createNavigation.ts
+++ frontend/src/elements/createNavigation.ts
@@ -1,3 +1,16 @@
+import { checkLoggedUser } from "../routing/redirect";
+
+const getProfileIcon = () => {
+	return `<img id="profileIcon" src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="Profile" class="w-12 h-12 rounded-full cursor-pointer">`
+}
+
+const getLoginBtn = () => {
+	return `<div id="loginBtn" alt="Profile" class="w-full h-8 cursor-pointer bg-green-300 flex items-center">
+			<h1>LOGIN</h1>
+	</div>`
+
+}
+
 export function navigation() {
 	return `
 		<nav class="top-0 left-0 bg-black text-white h-16 w-full fixed flex items-center px-6">
@@ -13,7 +26,8 @@ export function navigation() {
 
 
 			<div class="relative">
-        		<img id="profileIcon" src="https://img.icons8.com/fluency/48/test-account--v1.png" alt="Profile" class="w-12 h-12 rounded-full cursor-pointer">
+				${checkLoggedUser() ? getProfileIcon() : getLoginBtn()}
+        		
 
 
         		<div id="dropdownMenu" class="absolute right-0 mt-2 w-48 bg-white text-black shadow-lg rounded-lg p-2 hidden">
