4674725b3bef1ad0c04230844352f8ea457c596c
novsiann <nikitaovsannik63@gmail.com>
2025-03-17 22:17:36 +0100

    add logout ability, and changed onclick

--- frontend/src/nagivation/navigation.ts
+++ frontend/src/nagivation/navigation.ts
@@ -1,10 +1,12 @@
 import { navigateTo } from "../routing";
 import { naviRoutes } from "./navigationRoutes";
 import { dropMenuRoutes } from "./navigationRoutes";
+import { userChangeLoginStatus } from "../routing/redirect";
 
 export function navigationHandle() {
 	const naviBtns = document.querySelectorAll("#naviBtn");
 	const profileBtn = document.querySelector("#profileIcon");
+	const loginBtn = document.querySelector("#loginBtn");
 	const dropdownMenu = document.querySelector("#dropdownMenu");
 	const naviDropMenuBtns = document.querySelectorAll("#dropdownMenu a");
 	const imgLogo = document.getElementById('imgLogoNavi');
@@ -25,12 +27,20 @@ export function navigationHandle() {
 		btn.addEventListener('click', () => {
 			if (btn.innerHTML.trim() in dropMenuRoutes) {
 				navigateTo(dropMenuRoutes[btn.innerHTML.trim()]);
+				// shit-code, when database will be, this code-area has to be improved
+				if(btn.innerHTML.trim() === 'Logout'){
+					userChangeLoginStatus(false);
+				}
 			}
 		});
 	})
 
-	profileBtn!.addEventListener('click', () => {
+	profileBtn?.addEventListener('click', () => {
 		dropdownMenu!.classList.toggle("hidden");
+	});
+
+	loginBtn?.addEventListener('click', () => {
+		userChangeLoginStatus(true);	
 	});
 
 	document.addEventListener('click', (e) => {
