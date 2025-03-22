0365a73e6dff0444ede1d300be3cfbcfd1ee3cf9
novsiann <nikitaovsannik63@gmail.com>
2025-03-22 17:37:27 +0100

    change logic of spa func

--- frontend/src/spaApp/index.ts
+++ frontend/src/spaApp/index.ts
@@ -1,30 +1,39 @@
 import { mainWrapper } from "../elements";
-import { navigateTo, privateRoutes, publicRoutes} from "../routing/index";
+import {
+  allRoutes,
+  navigateTo,
+  privateRoutes,
+  publicRoutes,
+} from "../routing/index";
 import { checkLoggedUser } from "../routing/redirect";
-// import { errorPage } from "../Layout/index"
-// import { handleErrorPage } from '../pages/errorPage';
 
 export default function runSPA(path?: string) {
   document.body.append(mainWrapper);
 
-  if (location.pathname in privateRoutes) {
+  if (location.pathname in allRoutes) {
     mainWrapper.removeAttribute("id");
-    // mainWrapper.className = "flex flex-col h-full w-full";
     mainWrapper.className = "container mx-auto px-4 h-full w-full";
     document.body.className = "font-mono";
-    if (checkLoggedUser() && Object.keys(privateRoutes).includes(location.pathname)) {
-      mainWrapper.innerHTML = privateRoutes[location.pathname].layoutCreate(mainWrapper);
-      privateRoutes[location.pathname].handleFunc(mainWrapper);
+    if (
+      checkLoggedUser() &&
+      Object.keys(privateRoutes).includes(location.pathname)
+    ) {
+      mainWrapper.innerHTML =
+        privateRoutes[location.pathname].layoutCreate(mainWrapper);
+        privateRoutes[location.pathname].handleFunc(mainWrapper);
     } else {
-        if(Object.keys(publicRoutes).includes(location.pathname)){
-            mainWrapper.innerHTML = publicRoutes[location.pathname].layoutCreate(mainWrapper);
-            publicRoutes[location.pathname].handleFunc(mainWrapper);
-        }  else if(location.pathname !== '/')
-            navigateTo('/');
+      if (!checkLoggedUser() && Object.keys(publicRoutes).includes(location.pathname)) {
+          mainWrapper.innerHTML =
+          publicRoutes[location.pathname].layoutCreate(mainWrapper);
+        publicRoutes[location.pathname].handleFunc(mainWrapper);
+      } else {
+        navigateTo("/");
+      }
     }
   } else {
-    mainWrapper.innerHTML = privateRoutes["/error"].layoutCreate();
-    privateRoutes["/error"].handleFunc();
+    navigateTo("/error");
+    // mainWrapper.innerHTML = privateRoutes["/error"].layoutCreate();
+    // privateRoutes["/error"].handleFunc();
   }
 }
 
