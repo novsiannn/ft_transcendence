ce1016f1a560dfb2ba8bd0f62c2c2e3ec7c1f2d5
novsiann <nikitaovsannik63@gmail.com>
2025-03-22 21:17:57 +0100

    show to user that password or email is incorrect

--- frontend/src/pages/login/loginPage.ts
+++ frontend/src/pages/login/loginPage.ts
@@ -3,31 +3,41 @@ import { userDataLogin } from "../../shared";
 import { IUserDataTypeLogin } from "../../shared";
 import { userAPI } from "../../services/api";
 import { navigateTo } from "../../routing";
+import { activateWarning, loginPage } from "../../Layout";
 
-
-// Post datas !!! Delete userAPI if u are using fetch request
 export function handleLogin() {
-    const loginEmailInput = document.getElementById("loginInput") as HTMLInputElement;
-    const loginPassInput = document.getElementById("loginPass") as HTMLInputElement;;
-    const loginBtn = document.getElementById("loginBtn");
+  const loginEmailInput = document.getElementById(
+    "loginInput"
+  ) as HTMLInputElement;
+  const loginPassInput = document.getElementById(
+    "loginPass"
+  ) as HTMLInputElement;
+  const loginBtn = document.getElementById("loginBtn");
 
-    const LoginBtns: HTMLInputElement[] = [loginEmailInput, loginPassInput];
+  const LoginBtns: HTMLInputElement[] = [loginEmailInput, loginPassInput];
 
-    loginBtn!.addEventListener("click", (e) => {
-        e.preventDefault();
-        try {
-            LoginBtns.forEach((input) => {
-                const key = input.type.toLowerCase() as keyof IUserDataTypeLogin;
-                if (!input.value) {
-                    throw new Error("One of your inputs is empty!");
-                }
-                userDataLogin[key] = input.value;
-            });
-            console.log(userDataLogin);
-            userAPI.postDatas(userDataLogin, 'login');
-            navigateTo("/");
-        } catch (error) {
-            alert(error);
+  loginBtn!.addEventListener("click", async (e) => {
+    e.preventDefault();
+    try {
+      LoginBtns.forEach((input) => {
+        const key = input.type.toLowerCase() as keyof IUserDataTypeLogin;
+        if (input.type === "email") {
+          userDataLogin[key] = input.value.toLowerCase();
+        } else {
+          userDataLogin[key] = input.value;
         }
-    })
+      });
+      const response = await userAPI.postDatas(userDataLogin, "login");
+      if (response.status === 401) {
+        console.log('asd');
+        
+        activateWarning();
+      } else {
+        navigateTo("/");
+      }
+      console.log(response);
+    } catch (error) {
+      alert(error);
+    }
+  });
 }
