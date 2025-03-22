33b661449bc8af1307944b3a0f6f210b47b112f5
novsiann <nikitaovsannik63@gmail.com>
2025-03-22 17:37:01 +0100

    small changes

--- frontend/src/nagivation/navigation.ts
+++ frontend/src/nagivation/navigation.ts
@@ -4,53 +4,52 @@ import { dropMenuRoutes } from "./navigationRoutes";
 import { userChangeLoginStatus } from "../routing/redirect";
 
 export function navigationHandle() {
-	const naviBtns = document.querySelectorAll("#naviBtn");
-	const profileBtn = document.querySelector("#profileIcon");
-	const loginBtn = document.querySelector("#loginBtn");
-	const dropdownMenu = document.querySelector("#dropdownMenu");
-	const naviDropMenuBtns = document.querySelectorAll("#dropdownMenu a");
-	const imgLogo = document.getElementById('imgLogoNavi');
+  const naviBtns = document.querySelectorAll("#naviBtn");
+  const profileBtn = document.querySelector("#profileIcon");
+  const loginBtn = document.querySelector("#loginBtn");
+  const dropdownMenu = document.querySelector("#dropdownMenu");
+  const naviDropMenuBtns = document.querySelectorAll("#dropdownMenu a");
+  const imgLogo = document.getElementById("imgLogoNavi");
 
-	imgLogo!.addEventListener('click', () => {
-		navigateTo('/');
-	})
+  imgLogo!.addEventListener("click", () => {
+    navigateTo("/");
+  });
 
-	naviBtns.forEach((btn) => {
-		btn.addEventListener('click', () => {			
-			if (btn.innerHTML.trim() in naviRoutes) {
-				navigateTo(naviRoutes[btn.innerHTML.trim()]);
-			}
-		});
-	})
+  naviBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      if (btn.innerHTML.trim() in naviRoutes) {
+        navigateTo(naviRoutes[btn.innerHTML.trim()]);
+      }
+    });
+  });
 
-	naviDropMenuBtns.forEach((btn) => {
-		btn.addEventListener('click', () => {
-			if (btn.innerHTML.trim() in dropMenuRoutes) {
-				navigateTo(dropMenuRoutes[btn.innerHTML.trim()]);
-				// shit-code, when database will be, this code-area has to be improved
-				if(btn.innerHTML.trim() === 'Logout'){
-					userChangeLoginStatus(false);
-				}
-			}
-		});
-	})
+  naviDropMenuBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      if (btn.innerHTML.trim() in dropMenuRoutes) {
+        navigateTo(dropMenuRoutes[btn.innerHTML.trim()]);
+        // shit-code, when database will be, this code-area has to be improved
+        if (btn.innerHTML.trim() === "Logout") {
+          userChangeLoginStatus(false);
+        }
+      }
+    });
+  });
 
-	profileBtn?.addEventListener('click', () => {
-		dropdownMenu!.classList.toggle("hidden");
-	});
+  profileBtn?.addEventListener("click", () => {
+    dropdownMenu!.classList.toggle("hidden");
+  });
 
-	loginBtn?.addEventListener('click', () => {
-		userChangeLoginStatus(true);	
-	});
+  loginBtn?.addEventListener("click", () => {
+    userChangeLoginStatus(true);
+    navigateTo("/profile")
+  });
 
-	document.addEventListener('click', (e) => {
-		if (e.target !== dropdownMenu && e.target !== profileBtn)
-			dropdownMenu!.classList.add("hidden");
-	})
+  document.addEventListener("click", (e) => {
+    if (e.target !== dropdownMenu && e.target !== profileBtn)
+      dropdownMenu!.classList.add("hidden");
+  });
 
-	document.addEventListener('keydown', (e) => {
-		if (e.keyCode == 27)
-			dropdownMenu!.classList.add("hidden");
-	})
-
+  document.addEventListener("keydown", (e) => {
+    if (e.keyCode == 27) dropdownMenu!.classList.add("hidden");
+  });
 }
