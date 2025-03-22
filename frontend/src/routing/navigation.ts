506f4c96c61d4dcc909b5114a2a8f858d8d55560
novsiann <nikitaovsannik63@gmail.com>
2025-03-22 17:39:02 +0100

    small changes

--- frontend/src/routing/navigation.ts
+++ frontend/src/routing/navigation.ts
@@ -1,6 +1,11 @@
 import runSPA from "../spaApp";
 
-export function navigateTo(path: string) {
-    history.pushState({}, "", path); // Change URL without reload
-    runSPA(path); // Draw new Page
+export function navigateTo(newPath: string) {
+  console.log(newPath);
+  console.log(location.pathname);
+
+  if (newPath !== location.pathname) {
+    history.pushState({}, "", newPath); // Change URL without reload
+    runSPA(newPath); // Draw new Page
+  }
 }
