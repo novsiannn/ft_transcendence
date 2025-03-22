67ab34d13eb4d1a85c84a8b6aeba16a520a0eb52
novsiann <nikitaovsannik63@gmail.com>
2025-03-22 21:17:17 +0100

    improve code

--- frontend/src/pages/registration/registration.ts
+++ frontend/src/pages/registration/registration.ts
@@ -1,17 +1,17 @@
 import { navigateTo } from "../../routing/index"
-// import { postDatas } from "../../services/api";
 import { IUserDataRegistrationType } from "../../shared";
 import { userDataRegistration } from "../../shared";
 import { userAPI } from "../../services/api";
+import { validateInput } from "../../shared/validation";
 
-//changed from kilchenk
+//changed from novsiann
 export function handleRegistration() {
     const userNameInput = document.querySelector("#usernameRegistration") as HTMLInputElement;
     const emailInput = document.querySelector("#emailRegistration") as HTMLInputElement;
     const passwordInput = document.querySelector("#passwordRegistration") as HTMLInputElement;
     const btnRegistr = document.querySelector("#submitRegistration") as HTMLButtonElement;
     const signInBtn = document.querySelector('#questionAlreadyRegistr');
-
+    let username: null | string = null;
     const inputsUsers: HTMLInputElement[] = [userNameInput, emailInput, passwordInput];
 
     btnRegistr!.addEventListener('click', (e) => {
@@ -23,29 +23,14 @@ export function handleRegistration() {
                 password: null
             };
             inputsUsers.forEach(input => {
-                // const key = input.type.toLowerCase() as keyof IUserDataRegistrationType;
-                if (!input.value) {
-                    throw new Error("One of your inputs is empty!");
+                let key = input.type.toLowerCase() as keyof IUserDataRegistrationType;
+                validateInput(input);
+                if(input.type === 'text'){
+                    key = 'username';
                 }
-                if (input.value.length < 3) {
-                    throw new Error(`${input.placeholder} has to be minimum 3 characters`);
-                }
-                if (input.type === 'email' && !(input.value.match(/@/g))) {
-                    throw new Error(`${input.placeholder} has contain ' @ ' symbol`);
-                }
-                // userDataRegistration[key] = input.value;
-                if (input.id === 'usernameRegistration') {
-                    userDataRegistration.username = input.value;
-                }
-                else if (input.id === 'emailRegistration') {
-                    userDataRegistration.email = input.value;
-                }
-                else if (input.id === 'passwordRegistration') {
-                    userDataRegistration.password = input.value;
-                }
+                userDataRegistration[key] = input.value;
             });
             userAPI.postDatas(userDataRegistration, 'registration');
-            console.log(userDataRegistration);
             navigateTo("/activate");
         } catch (error) {
             alert(error);
