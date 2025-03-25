80666e96ddbe7c6f3590a911a452c04cd21b0afe
novsiann <nikitaovsannik63@gmail.com>
2025-03-25 23:06:07 +0100

    change registration code

--- frontend/src/pages/registration/registration.ts
+++ frontend/src/pages/registration/registration.ts
@@ -14,6 +14,40 @@ export function handleRegistration() {
     const signInBtn = document.querySelector('#questionAlreadyRegistr');
     const inputsUsers: HTMLInputElement[] = [userNameInput, emailInput, passwordInput];
 
+    const showModal = document.querySelector('#showModal');
+    const modalWindow = document.querySelector('#modalWindow');
+    const modalContent = modalWindow?.querySelector("div");
+    const closeModal = document.querySelector('#closeModal');
+
+    showModal?.addEventListener("click", () => {
+        modalWindow?.classList.remove("hidden");
+        setTimeout(() => {
+            modalContent?.classList.remove("-translate-y-full", "opacity-0");
+            modalContent?.classList.add("translate-y-0", "opacity-100");
+        }, 10);
+    });
+
+        closeModal?.addEventListener("click", () => {
+        modalContent?.classList.remove("translate-y-0", "opacity-100");
+        modalContent?.classList.add("-translate-y-full", "opacity-0");
+
+        setTimeout(() => {
+            modalWindow?.classList.add("hidden");
+        }, 500);
+    });
+
+    modalWindow?.addEventListener("click", (e) => {
+        if (e.target === modalWindow) {
+            modalContent?.classList.remove("translate-y-0", "opacity-100");
+            modalContent?.classList.add("-translate-y-full", "opacity-0");
+
+            setTimeout(() => {
+                modalWindow?.classList.add("hidden");
+            }, 500);
+        }
+    });
+
+
     navigationHandle();
 
     btnRegistr!.addEventListener('click', (e) => {
