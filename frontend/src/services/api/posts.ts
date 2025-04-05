f434112cc3a8c04c8c767197895df06dfe633292
novsiann <nikitaovsannik63@gmail.com>
2025-04-05 11:20:52 +0200

    add handle response in request

--- frontend/src/services/api/posts.ts
+++ frontend/src/services/api/posts.ts
@@ -1,4 +1,4 @@
-// FETCH REQUEST - - - - - - - - - - - - 
+import { handleRequests } from "./handleRequests";
 
 export const userAPI = {
 	postDatas: async (data: Object, endpoint: string) => {
@@ -9,7 +9,7 @@ export const userAPI = {
 					'Content-type': 'application/json;charset=utf-8'
 				},
 				body: JSON.stringify(data)
-			}).then( response => response );
+			}).then( response => handleRequests(response.status));
 	},
 
 	getDatas:  () => {
@@ -22,27 +22,3 @@ export const userAPI = {
 			.catch(error => console.log('Error fetching data: ' + error));
 	}
 }
-
-
-// AXIOS REQUEST - - - - - - - - - - - - 
-// console.log("axios works properly");
-
-// const instance = axios.create({
-// 	baseURL: "https://jsonplaceholder.typicode.com/posts",
-// });
-
-// export const userAPI = {
-// 	getDatas: () => {
-// 		//	change in future url (when database will be ready to use) 
-// 		return instance.get("")
-// 			.then(response => console.log(response.data))
-// 	},
-
-// 	postDatas: (data: Object) => {
-// 		//	change in future url (when database will be ready to use) 
-// 		instance.post("", data)
-// 			.then(response => console.log(response))
-// 			.then((result) => console.log("Data successfully delivered: with axios", result))
-// 			.catch((error) => console.log("Error posting data: " + error));
-// 	},
-// }
