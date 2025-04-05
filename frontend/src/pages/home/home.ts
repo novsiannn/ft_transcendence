f3be31e8975bb85ca2ed733280614296a6a1cd38
novsiann <nikitaovsannik63@gmail.com>
2025-04-05 11:54:13 +0200

    add modal success to main page

--- frontend/src/pages/home/home.ts
+++ frontend/src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { handleModalSuccess } from "../../elements/ModalSuccess";
 import { navigationHandle } from "../../nagivation";
 
 export function handleHomePage(mainWrapper: HTMLDivElement | undefined) {
