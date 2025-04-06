789f88d28dae103a384ab4d89be84da43cec2842
novsiann <nikitaovsannik63@gmail.com>
2025-04-06 11:26:32 +0200

    change condition

--- frontend/src/spaApp/index.ts
+++ frontend/src/spaApp/index.ts
@@ -5,24 +5,26 @@ import {
   privateRoutes,
   publicRoutes,
 } from "../routing/index";
-import { checkLoggedUser } from "../routing/redirect";
+import { store } from "../store/store";
 
 export default function runSPA() {
   document.body.append(mainWrapper);
 
   if (location.pathname in allRoutes) {
+    console.log(store.getState());
+    
     mainWrapper.removeAttribute("id");
     mainWrapper.className = "h-full w-full";
     document.body.className = "font-mono";
     if (
-      checkLoggedUser() &&
+      store.getAuth() &&
       Object.keys(privateRoutes).includes(location.pathname)
     ) {
       mainWrapper.innerHTML =
         privateRoutes[location.pathname].layoutCreate(mainWrapper);
         privateRoutes[location.pathname].handleFunc(mainWrapper);
     } else {
-      if (!checkLoggedUser() && Object.keys(publicRoutes).includes(location.pathname)) {
+      if (!store.getAuth() && Object.keys(publicRoutes).includes(location.pathname)) {
           mainWrapper.innerHTML =
           publicRoutes[location.pathname].layoutCreate(mainWrapper);
         publicRoutes[location.pathname].handleFunc(mainWrapper);
