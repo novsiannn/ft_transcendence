51bbc00f87b069bc926b54d6053cc2b310330375
novsiann <nikitaovsannik63@gmail.com>
2025-04-15 11:31:20 +0200

    change modalTwoFactor

--- frontend/src/elements/ModalTwoFactor/handleModalTwoFactor.ts
+++ frontend/src/elements/ModalTwoFactor/handleModalTwoFactor.ts
@@ -1,35 +1,51 @@
-export const handleModalTwoFactor = () => {
+import { activateWarning, hideWarning } from "../../Layout";
+import { IQRCodeEnableResponse } from "../../shared";
+import { store } from "../../store/store";
+
+export const handleModalTwoFactor = async () => {
   const modalWindow = document.querySelector("#modalWindowTwoFactor");
   const modalContent = modalWindow?.querySelector("div");
-  const qrCodeImg = modalWindow?.querySelector<HTMLImageElement>("#modalQrCode");
+  const qrCodeImg =
+    modalWindow?.querySelector<HTMLImageElement>("#modalQrCode");
   const secretPhrase = modalWindow?.querySelector("#secretPhrase");
-  const codeForTwoFactor = modalWindow?.querySelector("#codeForTwoFactor");
+  const codeForTwoFactor = modalWindow?.querySelector<HTMLInputElement>("#codeForTwoFactor");
   const modalBtn = modalWindow?.querySelector("#modalTwoFactorActivateBtn");
-  
-  console.log(modalWindow);
+  const response: IQRCodeEnableResponse | undefined =
+    await store.enableTwoFactor();
+  const code = response?.secret;
 
-  const code = "77732821943"
+  qrCodeImg!.src = response?.qrCodeUrl ? response?.qrCodeUrl : "undefined";
+  code
+    ? (secretPhrase!.textContent = code)
+    : (secretPhrase!.textContent = "Loading error...");
 
-  qrCodeImg!.src = '../../img/qrCode.png';
-  secretPhrase!.textContent = code;
-  
   modalWindow?.classList.remove("hidden");
   setTimeout(() => {
     modalContent?.classList.remove("-translate-y-full", "opacity-0");
     modalContent?.classList.add("translate-y-0", "opacity-100");
   }, 50);
 
-  modalBtn?.addEventListener("click", () => {
-    // Here make a request to backend and then make next handling
-    modalContent?.classList.remove("translate-y-0", "opacity-100");
-    modalContent?.classList.add("-translate-y-full", "opacity-0");
-    setTimeout(() => {
-      modalWindow?.classList.add("hidden");
-    }, 400);
+  codeForTwoFactor?.addEventListener('click', () => hideWarning());
+
+  modalBtn?.addEventListener("click", async () => {
+    hideWarning();
+    const response = await store.verifyTwoFactor(
+      codeForTwoFactor?.value ? codeForTwoFactor.value : ""
+    );
+    if (response.status === 200) {
+      modalContent?.classList.remove("translate-y-0", "opacity-100");
+      modalContent?.classList.add("-translate-y-full", "opacity-0");
+      setTimeout(() => {
+        modalWindow?.classList.add("hidden");
+      }, 400);
+    } else if (response.status === 400){
+      activateWarning(response.data.message);
+    }
   });
 
   modalWindow?.addEventListener("click", (e) => {
     if (e.target === modalWindow) {
+      hideWarning();
       modalContent?.classList.remove("translate-y-0", "opacity-100");
       modalContent?.classList.add("-translate-y-full", "opacity-0");
 
