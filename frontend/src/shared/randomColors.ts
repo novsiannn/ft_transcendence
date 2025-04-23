9a16012b9c6b9e6a7ed11eb9b8e8ed582023bf0d
novsiann <nikitaovsannik63@gmail.com>
2025-04-23 23:31:29 +0200

    add random colors'

---
+++ frontend/src/shared/randomColors.ts
@@ -0,0 +1,27 @@
+export const getColorFromUsername = (username: string) => {
+  const colors = [
+    'bg-red-500',
+    'bg-blue-500',
+    'bg-green-500',
+    'bg-yellow-500',
+    'bg-purple-500',
+    'bg-pink-500',
+    'bg-indigo-500',
+    'bg-teal-500',
+    'bg-orange-500',
+    'bg-cyan-500',
+    'bg-lime-500',
+    'bg-rose-500',
+    'bg-emerald-500',
+    'bg-violet-500',
+    'bg-sky-500',
+  ];
+
+  if (!username) return colors[0];
+
+  const firstChar = username.trim().toLowerCase().charCodeAt(0);
+
+  const index = firstChar % colors.length;
+
+  return colors[index];
+};
