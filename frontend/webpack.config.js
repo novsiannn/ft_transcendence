0b715f07a4262b6e1d45f1b9148abaa05560568f
novsiann <nikitaovsannik63@gmail.com>
2025-04-25 11:36:00 +0200

    new dependencies + new configuration

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,36 +1,40 @@
-const path = require('path');
-const fs = require('fs');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // Плагин для копирования index.html и добавления скриптов
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // Плагин для копирования index.html и добавления скриптов
 
-const keyPath = path.join(__dirname, '/certs/key.pem'); // added by kilchenk
-const certPath = path.join(__dirname, '/certs/cert.pem'); // added by kilchenk
+const keyPath = path.join(__dirname, "/certs/key.pem"); // added by kilchenk
+const certPath = path.join(__dirname, "/certs/cert.pem"); // added by kilchenk
 
 const isHttps = fs.existsSync(keyPath) && fs.existsSync(certPath); // added by kilchenk
 
 module.exports = {
-  entry: './src/index.ts', // Входной файл
-  devtool: 'inline-source-map', // Для отладки (показывает исходные файлы)
-  mode: 'development', // IF PROGRAMM CRASHS DELETE THIS LINE !!!!!!!!!!!!!!!!!!!!!!!!
+  entry: "./src/index.ts", // Входной файл
+  devtool: "inline-source-map", // Для отладки (показывает исходные файлы)
+  mode: "development", // IF PROGRAMM CRASHS DELETE THIS LINE !!!!!!!!!!!!!!!!!!!!!!!!
   module: {
     rules: [
       {
-        test: /\.tsx?$/, // Обрабатываем файлы TypeScript
-        use: 'ts-loader',
+        test: /\.tsx?$/,
+        use: "ts-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'], // Расширения, которые Webpack будет обрабатывать
+    extensions: [".tsx", ".ts", ".js"], // Расширения, которые Webpack будет обрабатывать
   },
   output: {
-    filename: 'script.js', // Название собранного файла
-    path: path.resolve(__dirname, 'dist'), // Папка dist
-    publicPath: '/', // Путь для правильной работы с сервером
+    filename: "script.js", // Название собранного файла
+    path: path.resolve(__dirname, "dist"), // Папка dist
+    publicPath: "/", // Путь для правильной работы с сервером
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'), // Отдаем файлы из папки dist
+      directory: path.join(__dirname, "dist"), // Отдаем файлы из папки dist
     },
     compress: true, // added by kilchenk
     historyApiFallback: true, // Поддержка SPA
@@ -39,18 +43,20 @@ module.exports = {
     open: true, // Автоматически откроется браузер
 
     //added by kilchenk from
-    server: isHttps ? {
-      type: 'https',
-      options: {
-        key: fs.readFileSync(keyPath),
-        cert: fs.readFileSync(certPath),
-      }
-    } : 'http',
+    server: isHttps
+      ? {
+          type: "https",
+          options: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certPath),
+          },
+        }
+      : "http",
     //added by kilchenk to
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html', // Используем шаблон index.html из папки src
+      template: "./src/index.html", // Используем шаблон index.html из папки src
     }),
   ],
 };
