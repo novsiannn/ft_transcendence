3c5a513cd045532d8524c1fb36caeac644b02ddd
novsiann <nikitaovsannik63@gmail.com>
2025-04-25 11:43:04 +0200

    change index.ts/html for a new tailwind version

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,5 +1,6 @@
 import runSPA from './spaApp/index'
 import { store } from './store/store';
+import './output.css';
 
 window.addEventListener("DOMContentLoaded", async () => {
     await store.checkAuth();
