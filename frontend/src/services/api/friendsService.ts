b2543421fd95aba06e3b3724c7849c1ad09d2a8d
novsiann <nikitaovsannik63@gmail.com>
2025-04-25 11:57:34 +0200

    add requests to friends service

--- frontend/src/services/api/friendsService.ts
+++ frontend/src/services/api/friendsService.ts
@@ -1,13 +1,19 @@
-import { IFriend } from "../../shared";
+import { IFriend, IFriendsResponse } from "../../shared";
 import instanceAPI from "./instanceAxios";
 
 const friendsService = {
   getFriends: async () => {
-	return await instanceAPI.get<IFriend[]>("friendship");
+	return await instanceAPI.get<IFriendsResponse>("friendship");
   },
   sendFriendRequest: async (addresseeId: number) => {
     return await instanceAPI.post<IFriend[]>("friendship", {addresseeId});
-  }
+  },
+  getPendingFriendsRequests : async () => {
+    return await instanceAPI.get("friendship/sent-pending");
+  },
+  cancelPendingFriendRequest : async (friendshipId: number) => {
+    return await instanceAPI.delete(`friendship/outgoing/${friendshipId}`);
+  },
 };
 
 export default friendsService;
