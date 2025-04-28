21c09174af0e9c2fbc474e08ac51cf9540bf62cc
Illia <robot13673@gmail.com>
2025-04-28 20:39:05 +0200

    Added chat.js for websockets

---
+++ backend/src/socket/handlers/chat.js
@@ -0,0 +1,4 @@
+const userTracker = require('../utils/userTracker');
+const { chatController } = require('../../controllers/chat.controller.js');
+const Message = require('../../../db/models/MessageModel');
+const Chat = require('../../../db/models/ChatModel');
