5ff8b808b9b78dadc3e93cb892ef9539d11d060b
novsiann <nikitaovsannik63@gmail.com>
2025-04-30 00:15:15 +0200

    change id of element

--- frontend/src/elements/ModalSuccess/layoutModalSuccess.ts
+++ frontend/src/elements/ModalSuccess/layoutModalSuccess.ts
@@ -1,5 +1,5 @@
 export const getModalWindowSuccess = () => {
-  return `<div id="modalWindow" class="fixed inset-0 hidden flex justify-center transition-all duration-500">
+  return `<div id="modalWindowSucess" class="fixed inset-0 hidden flex justify-center transition-all duration-500">
               <div class="flex mt-12 flex-col justify-center items-center bg-white w-60 h-24 text-white p-6 rounded-lg shadow-lg opacity-0 transform -translate-y-full transition-all duration-500">
                   <div class="flex justify-center mt-4">
                       <svg class="w-8 h-8 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
