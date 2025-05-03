8b497d6deb80274c6158f9084ce4386b3fdb0082
novsiann <nikitaovsannik63@gmail.com>
2025-05-03 12:50:57 +0200

    fix error with request

--- frontend/src/services/api/instanceAxios.ts
+++ frontend/src/services/api/instanceAxios.ts
@@ -21,6 +21,11 @@ instanceAPI.interceptors.response.use(
   },
   async (error) => {
     const originalRequest = error.config;
+
+    if (!error.response) {
+      return Promise.reject(error);
+    }
+    
     if (
       error.response.status === 401 &&
       error.response.data?.requiresTwoFactor &&
