7b1467b0d0e250dad6b772df4a2576adecd3ca2b
novsiann <nikitaovsannik63@gmail.com>
2025-05-07 12:16:30 +0200

    add right name to folder

---
+++ frontend/src/elements/navigation/navigationRoutes.ts
@@ -0,0 +1,8 @@
+export const dropMenuRoutes: { [key: string]: string } = {
+	"profileDropNavi": "/profile",
+	"gameDropNavi": "/game",
+	"leaderboardDropNavi": "/leaderboard",
+	"settingsDropNavi": "/settings",
+	"friendsDropNavi": "/friends",
+	"chatsDropNavi": "/chats",
+}  
