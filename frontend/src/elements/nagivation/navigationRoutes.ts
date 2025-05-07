3c576c504187ba3d964c41be9b1a1c2600e8c6ff
novsiann <nikitaovsannik63@gmail.com>
2025-05-07 12:17:27 +0200

    delete navi with false name

--- frontend/src/elements/nagivation/navigationRoutes.ts
+++
@@ -1,8 +0,0 @@
-export const dropMenuRoutes: { [key: string]: string } = {
-	"profileDropNavi": "/profile",
-	"gameDropNavi": "/game",
-	"leaderboardDropNavi": "/leaderboard",
-	"settingsDropNavi": "/settings",
-	"friendsDropNavi": "/friends",
-	"chatsDropNavi": "/chats",
-}  
