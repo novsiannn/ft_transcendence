57c09996a2aad94823b4df5879f74adb5d3d7c6d
novsiann <nikitaovsannik63@gmail.com>
2025-05-24 21:31:47 +0200

    add Btns

---
+++ frontend/src/elements/BtnAdd/BtnAdd.ts
@@ -0,0 +1,3 @@
+export const BtnAdd = () => {
+  return `<button data-i18n='buttons.add' id="btnAddFriend" type="button" class=" p-1 hidden  px-4 h-8 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm  text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>`;
+};
---
+++ frontend/src/elements/BtnReject/BtnReject.ts
@@ -0,0 +1,3 @@
+export const BtnReject = () => {
+  return `<button data-i18n='buttons.reject' id="btnRejectFriend" type="button" class="hidden  px-4 h-8 text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm text-center  dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Reject</button>`;
+};
