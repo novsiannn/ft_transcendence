9d68f535454b6fa6215117af7fc24f014cef4c76
novsiann <nikitaovsannik63@gmail.com>
2025-05-30 15:22:13 +0200

    add and fix translation to all btns

--- frontend/src/elements/BtnUnblock/BtnUnblock.ts
+++ frontend/src/elements/BtnUnblock/BtnUnblock.ts
@@ -1,3 +1,5 @@
+import i18next from "i18next";
+
 export const BtnUnblock = () => {
-  return `<button data-i18n='buttons.unblock' id="btnUnblockUser" type="button" class=" cursor-pointer hidden px-4 h-8 text-green-600 bg-whit border border-green-600 font-medium rounded-full text-sm text-center ">Unblock</button>`;
+  return `<button id="btnUnblockUser" type="button" class=" cursor-pointer hidden px-4 h-8 text-green-600 bg-whit border border-green-600 font-medium rounded-full text-sm text-center ">${i18next.t("buttons.unBlock")}</button>`;
 };
--- frontend/src/elements/BtnUserBlockedYou/BtnUserBlockedYou.ts
+++ frontend/src/elements/BtnUserBlockedYou/BtnUserBlockedYou.ts
@@ -1,4 +1,6 @@
+import i18next from "i18next";
+
 export const BtnUserBlockedYou = () => {
-  return `<button data-i18n='buttons.accept' id="btnUserBlockedYou" type="button" class="cursor-not-allowed
- hidden  px-4 h-8 text-red-500 border border-red-500 font-medium rounded-full text-sm text-center">User Blocked You</button>`;
+  return `<button id="btnUserBlockedYou" type="button" class="cursor-not-allowed
+ hidden  px-4 h-8 text-red-500 border border-red-500 font-medium rounded-full text-sm text-center">${i18next.t("buttons.userBlocked")}</button>`;
 };
