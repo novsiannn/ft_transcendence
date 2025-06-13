fa15a0694a1fe3a59ea76dda7e19395200ed9f7a
Illia <robot13673@gmail.com>
2025-06-13 21:57:30 +0200

    Cancellation the game after 30 sec

--- backend/db/models/PinPongModel.js
+++ backend/db/models/PinPongModel.js
@@ -43,7 +43,7 @@ const PinPong = sequelize.define("PinPong", {
         defaultValue: 0,
     },
     status: {
-        type: DataTypes.ENUM('waiting', 'playing', 'finished', 'abandoned'),
+        type: DataTypes.ENUM('waiting', 'playing', 'finished', 'cancelled'),
         defaultValue: 'waiting',
         allowNull: false
     },
